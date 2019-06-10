7d392ba0f57cf62a843abd8fc8b9244b52fff746
Michael Gant <jmgant@cleareyeconsulting.com>
2019-06-10 14:02:09 -0400

    Trying to get migrate to work on remote

--- qmessentials-standard-php/app/Providers/AuthServiceProvider.php
+++ qmessentials-standard-php/app/Providers/AuthServiceProvider.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -31,53 +32,65 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        if (Schema::hasTable('roles')) {
 
-        Gate::before(function($user, $ability) {
-            if ($this->isUserInRole($user, ['Administrator'])) {
-                return true;
-            }
-        });
+            Gate::before(function($user, $ability) {
+                if ($this->isUserInRole($user, ['Administrator'])) {
+                    return true;
+                }
+            });
 
-        Gate::define('write-metric', function($user) {
-            return $this->isUserInRole($user, ['Analyst']);
-        });
+            Gate::define('write-metric', function($user) {
+                return $this->isUserInRole($user, ['Analyst']);
+            });
 
-        Gate::define('read-metric', function($user) {
-            return $this->isUserInRole($user, ['Analyst', 'Technician']);
-        });
+            Gate::define('read-metric', function($user) {
+                return $this->isUserInRole($user, ['Analyst', 'Technician']);
+            });
 
-        Gate::define('write-test-plan', function($user) {
-            return $this->isUserInRole($user, ['Analyst']);
-        });
+            Gate::define('write-test-plan', function($user) {
+                return $this->isUserInRole($user, ['Analyst']);
+            });
 
-        Gate::define('write-product', function($user) {
-            return $this->isUserInRole($user, ['Analyst']);
-        });
+            Gate::define('write-product', function($user) {
+                return $this->isUserInRole($user, ['Analyst']);
+            });
 
-        Gate::define('write-user', function($user) {
-            return $this->isUserInRole($user, ['Administrator']);
-        });
+            Gate::define('write-user', function($user) {
+                return $this->isUserInRole($user, ['Administrator']);
+            });
 
-        Gate::define('write-lot', function($user) {
-            return $this->isUserInRole($user, ['Lead Person']);
-        });
+            Gate::define('write-lot', function($user) {
+                return $this->isUserInRole($user, ['Lead Person']);
+            });
 
-        Gate::define('write-observation', function($user) {
-            return $this->isUserInRole($user, ['Technician']);
-        });
+            Gate::define('write-observation', function($user) {
+                return $this->isUserInRole($user, ['Technician']);
+            });
 
-        Gate::define('read-aggregate-data', function($user) {
-            return $this->isUserInRole($user, ['Quality Manager']);
-        });
-        $this->bootstrapRoles();
-        $this->bootstrapAdminUser();
+            Gate::define('read-aggregate-data', function($user) {
+                return $this->isUserInRole($user, ['Quality Manager']);
+            });
+            $this->bootstrapRoles();
+            $this->bootstrapAdminUser();
+        }
+        else {
+            Gate::before(function($user, $ability) {
+                return false;
+            });
+        }
 
     }
 
     private function isUserInRole($user, $roleNames) {
-        $roleIdsForUser = UserRole::where('user_id', $user->id)->pluck('role_id')->toArray();
-        $roleIdsRequired = Role::whereIn('role_name', $roleNames)->pluck('id')->toArray();
-        return count(array_intersect($roleIdsForUser, $roleIdsRequired)) > 0;
+        foreach ($user->roles()->get() as $role) {
+            foreach ($roleNames as $roleName) {
+                if ($roleName == $role->role_name) {
+                    return true;
+                }
+            }
+        }
+        return false;        
     }
 
     private function bootstrapRoles() {
