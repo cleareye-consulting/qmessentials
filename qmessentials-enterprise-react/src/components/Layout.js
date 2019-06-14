8eb1a4881425466a8fc1d4625e0d01c7df3ff6bb
Michael Gant <jmgant@cleareyeconsulting.com>
2019-06-14 12:04:49 -0400

    Changes to login screen in enterprise

--- qmessentials-enterprise-react/src/components/Layout.js
+++ qmessentials-enterprise-react/src/components/Layout.js
@@ -4,21 +4,28 @@ import { Link } from 'react-router-dom'
 export default class Layout extends Component {
     render() {
         return (
-            <div className="container">
+            <React.Fragment>                    
                 <header>
-                    <nav className="navbar navbar-expand-md navbar-light bg-light">
-                        <Link className="navbar-brand" to="/">QM Essentials Enterprise</Link>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                    </nav>                    
+                    <nav className="navbar navbar-expand-md navbar-dark bg-primary">
+                        <div className="container">
+                            <Link className="navbar-brand" to="/">QM Essentials Enterprise</Link>
+                            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                        </div>
+                    </nav>
                 </header>
                 <main>
-                    {this.props.children}
+                    <div className="container">
+                        {this.props.children}
+                    </div>
                 </main>
                 <footer>
+                    
                 </footer>
-            </div>
+                        
+
+            </React.Fragment>
         )        
     }
 }
--- qmessentials-enterprise-react/src/components/auth/Login.js
+++ qmessentials-enterprise-react/src/components/auth/Login.js
@@ -45,25 +45,36 @@ export default class Login extends Component {
     render() {
         return (
             <React.Fragment>
-                <div className="form">
-                    <div className="form-group">
-                        <label className="control-label" htmlFor="userId">User ID</label>
-                        <input type="text" className="form-control" id="userId" name="userId" onChange={this.handleChange} />
+                    <div className="row justify-content-center">
+        <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">
+                            Login
+                        </div>
+                        <div className="card-body">
+                            <form className="form">
+                                <div className="form-group">
+                                    <label className="control-label" htmlFor="userId">User ID</label>
+                                    <input type="text" className="form-control" id="userId" name="userId" onChange={this.handleChange} />
+                                </div>
+                                <div className="form-group">
+                                    <label className="control-label" htmlFor="password">Password</label>
+                                    <input type="password" className="form-control"id="password" name="password" onChange={this.handleChange}/>
+                                </div>
+                                <div className="form-group">
+                                    <button className="btn btn-primary" id="submitButton" onClick={this.handleSubmit}>Log In</button>
+                                </div>
+                                {
+                                    this.state.failed
+                                        ? <div className="alert alert-danger" role="alert">There was a problem with your login credentials.</div>
+                                        : null
+                                }
+                            </form>
+                        </div>
+                        </div>    
                     </div>
-                    <div className="form-group">
-                        <label className="control-label" htmlFor="password">Password</label>
-                        <input type="password" className="form-control"id="password" name="password" onChange={this.handleChange}/>
                     </div>
-                    <div className="form-group">
-                        <button className="btn btn-primary" id="submitButton" onClick={this.handleSubmit}>Log In</button>
-                    </div>
-                    {
-                        this.state.failed
-                            ? <div className="alert alert-danger" role="alert">There was a problem with your login credentials.</div>
-                            : null
-                    }
-                    
-                </div>
+                
             </React.Fragment>
         )
     }
