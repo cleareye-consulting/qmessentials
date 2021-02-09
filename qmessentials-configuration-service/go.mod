63450e92da7847ff075660e6aed0a01019240a47
John Gant <johngant@Johns-MacBook-Pro-2.local>
2021-02-09 15:22:56 -0500

    Added some ORM code

--- qmessentials-configuration-service/go.mod
+++ qmessentials-configuration-service/go.mod
@@ -5,5 +5,6 @@ go 1.15
 require (
 	github.com/go-chi/chi v1.5.1
 	github.com/go-chi/render v1.0.1
+	github.com/go-pg/pg/v10 v10.7.5
 	github.com/rs/zerolog v1.20.0
 )
--- qmessentials-configuration-service/repositories/productRepository.go
+++ qmessentials-configuration-service/repositories/productRepository.go
@@ -4,26 +4,68 @@ import (
 	"errors"
 
 	"github.com/cleareyeconsulting/qmessentials/configuration/models"
+	"github.com/go-pg/pg/v10"
 )
 
+//ProductRepository represents the persistence layer for products
 type ProductRepository struct{}
 
-func (pr *ProductRepository) List() (*models.Product, error) {
-	return nil, errors.New("Not implemented")
+//List all products
+func (pr *ProductRepository) List() (*[]models.Product, error) {
+	db := pg.Connect(&pg.Options{
+		User: "postgres",
+	})
+	defer db.Close()
+	var products []models.Product
+	err := db.Model(products).Select()
+	return &products, err
 }
 
-func (pr *ProductRepository) Select(productID string) ([]*models.Product, error) {
-	return nil, errors.New("Not implemented")
+//Select a product by ID
+func (pr *ProductRepository) Select(productID string) (*models.Product, error) {
+	db := pg.Connect(&pg.Options{
+		User: "postgres",
+	})
+	defer db.Close()
+	product := models.Product{
+		ProductID: productID,
+	}
+	err := db.Model(product).WherePK().Select()
+	return &product, err
 }
 
+//Add a product
 func (pr *ProductRepository) Add(product *models.Product) error {
-	return errors.New("Not implemented")
+	db := pg.Connect(&pg.Options{
+		User: "postgres",
+	})
+	defer db.Close()
+	_, err := db.Model(&product).Insert()
+	return err
 }
 
+//Update a product
 func (pr *ProductRepository) Update(productID string, product *models.Product) error {
-	return errors.New(("Not implemented"))
+	if &product.ProductID != &productID {
+		return errors.New("Product ID mismatch")
+	}
+	db := pg.Connect(&pg.Options{
+		User: "postgres",
+	})
+	defer db.Close()
+	_, err := db.Model(&product).Update()
+	return err
 }
 
+//Delete a product
 func (pr *ProductRepository) Delete(productID string) error {
-	return errors.New("Not implemented")
+	db := pg.Connect(&pg.Options{
+		User: "postgres",
+	})
+	defer db.Close()
+	product := models.Product{
+		ProductID: productID,
+	}
+	_, err := db.Model(product).Delete()
+	return err
 }
