fcdc24116471b8f2a5bd138624b638a9e7a293ab
Michael Gant <jmgant@cleareyeconsulting.com>
2021-03-09 08:54:25 -0500

    Stubbed out routes for observation service

--- qmessentials-observation-service/go.mod
+++ qmessentials-observation-service/go.mod
@@ -1,3 +1,10 @@
 module github.com/cleareyeconsulting/qmessentials/observations
 
 go 1.16
+
+require (
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/go-pg/pg/v10 v10.8.0 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/rs/zerolog v1.20.0 // indirect
+)
--- qmessentials-observation-service/main.go
+++ qmessentials-observation-service/main.go
@@ -1,7 +1,22 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/joho/godotenv"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func init() {
+	godotenv.Load()
+}
+
+func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	log.Debug().Msg("Started")
 }
 
 //observations POST store to database then post to broker
