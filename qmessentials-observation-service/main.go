d86eef987fbedc4eb6ad30775fe3dee014ed0cfc
Michael Gant <jmgant@cleareyeconsulting.com>
2021-03-09 08:54:34 -0500

    Stubbed out routes for observation service

--- qmessentials-observation-service/main.go
+++ qmessentials-observation-service/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -17,8 +19,26 @@ func main() {
 	zerolog.SetGlobalLevel(zerolog.DebugLevel)
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	log.Debug().Msg("Started")
+
+	r := chi.NewRouter()
+	r.Post("/observations", handlePostObservation)
+	r.Get("/observations", handleGetObservations)
+
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		port = "5000"
+	}
+
+	http.ListenAndServe(":"+port, r)
+
 }
 
 //observations POST store to database then post to broker
+func handlePostObservation(w http.ResponseWriter, r *http.Request) {
+	panic("Not implemented")
+}
 
 //observations GET by lot
+func handleGetObservations(w http.ResponseWriter, r *http.Request) {
+	panic("Not implemented")
+}
