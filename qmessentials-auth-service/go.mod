faa51ba3e1e6583e9a891354274327881362f7b1
Michael Gant <jmgant@cleareyeconsulting.com>
2021-03-23 18:47:27 -0400

    Added SendGrid integration

--- qmessentials-auth-service/go.mod
+++ qmessentials-auth-service/go.mod
@@ -10,6 +10,8 @@ require (
 	github.com/joho/godotenv v1.3.0
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/rs/zerolog v1.20.0
+	github.com/sendgrid/rest v2.6.3+incompatible // indirect
+	github.com/sendgrid/sendgrid-go v3.8.0+incompatible // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 	go.mongodb.org/mongo-driver v1.5.0
 	golang.org/x/crypto v0.0.0-20210218145215-b8e89b74b9df
--- qmessentials-auth-service/utilities/emailUtil.go
+++ qmessentials-auth-service/utilities/emailUtil.go
@@ -1,19 +1,32 @@
 package utilities
 
 import (
+	"errors"
 	"net/smtp"
 	"os"
 
 	"github.com/rs/zerolog/log"
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
 type EmailUtil struct{}
 
 func (eu *EmailUtil) SendEmail(recipient string, subject string, body string) error {
-	emailUser := os.Getenv("EMAIL_USER")
-	emailPassword := os.Getenv("EMAIL_PASSWORD")
-	emailHost := os.Getenv("EMAIL_HOST")
-	emailServer := os.Getenv("EMAIL_HOST")
+	if os.Getenv("SMTP_HOST") != "" {
+		return sendEmailWithSmtp(recipient, subject, body)
+	}
+	if os.Getenv("SENDGRID_API_KEY") != "" {
+		return sendEmailWithSendGrid(recipient, subject, body)
+	}
+	return errors.New("unable to determine email method")
+}
+
+func sendEmailWithSmtp(recipient string, subject string, body string) error {
+	emailUser := os.Getenv("SMTP_USER")
+	emailPassword := os.Getenv("SMTP_PASSWORD")
+	emailHost := os.Getenv("SMTP_HOST")
+	emailServer := os.Getenv("SMTP_HOST")
 	if emailUser == "" || emailPassword == "" || emailHost == "" || emailServer == "" {
 		log.Warn().Msgf("Email settings not provided; email will not be sent. Body was %s", body)
 	}
@@ -21,5 +34,14 @@ func (eu *EmailUtil) SendEmail(recipient string, subject string, body string) er
 	to := []string{recipient}
 	bodyBytes := []byte(body)
 	err := smtp.SendMail(emailServer, auth, emailUser, to, bodyBytes)
+	return err
+}
+
+func sendEmailWithSendGrid(recipient string, subject string, body string) error {
+	from := mail.NewEmail(os.Getenv("SENDGRID_SENDER_NAME"), os.Getenv("SENDGRID_SENDER_EMAIL"))
+	to := mail.NewEmail(recipient, recipient)
+	message := mail.NewSingleEmailPlainText(from, subject, to, body)
+	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+	_, err := client.Send(message)
 	return err
 }
