531c8e8fd992c4e9f3359964826c7fcb0408126d
Michael Gant <jmgant@cleareyeconsulting.com>
2022-04-08 15:52:44 -0400

    Converted client application to typescript

--- qmessentials-client/src/components/layout/NavBar.js
+++
@@ -1,7 +0,0 @@
-export default function NavBar({ children }) {
-  return (
-    <nav className="navbar navbar-expand-lg">
-      <div className="container-fluid">{children}</div>
-    </nav>
-  )
-}
--- qmessentials-client/src/components/layout/NavBarMainMenu.js
+++
@@ -1,3 +0,0 @@
-export default function NavBarMainMenu({ children }) {
-  return <ul className="navbar-nav me-right mb-2 mb-lg-0">{children}</ul>
-}
