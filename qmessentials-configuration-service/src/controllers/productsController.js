dcda453dfdf8e00e09a671a887ef3e804fdfe420
Michael Gant <jmgant@cleareyeconsulting.com>
2022-05-13 14:09:00 -0400

    Removed first node version of configuration service, renamed Go version

--- qmessentials-configuration-service/src/controllers/productsController.js
+++
@@ -1,7 +0,0 @@
-import { listProducts } from '../repositories/products.js'
-
-export async function list(req, res) {
-  const activeOnly = req.query ? req.query.activeOnly : null
-  const results = await listProducts({ activeOnly })
-  res.send(results)
-}
--- qmessentials-configuration-service/src/routers/productsRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { list } from '../controllers/productsController.js'
-
-const router = new Router()
-
-router.get('/', list)
-
-export default router
